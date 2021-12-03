c302cd120298848f1d31c37393a7912eb0d86c43
Sydney Adjou-Moumouni <Sydney.ADJOU-MOUMOUNI@cpexterne.org>
2021-12-03 12:48:52 +0100

    Step 4

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,17 +1,39 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class StringCalculator {
+
+    //start with => //
+    // ([^0-9]) => the delimiter is everything except numbers
+    //\n => the line separator
+    // infinite chars
+    public static final String REGEX = "//([^0-9])\n(.*)";
 
     public int Add(String numbers) {
         if (null == numbers || numbers.isBlank()){
             return 0;
         } else {
-            List<Integer> numbersList = Arrays.stream(numbers.split(","))
+            List<Integer> numbersList = Arrays.stream(getSplit(numbers))
                     .map(Integer::parseInt)
                     .collect(Collectors.toList());
             return numbersList.stream().reduce(Integer::sum).orElse(0);
         }
+    }
+
+
+    private String[] getSplit(String delimiterAndNumbers) {
+        if(delimiterAndNumbers.startsWith("//")) {
+            Matcher matcher = Pattern.compile(REGEX).matcher(delimiterAndNumbers);
+            if (matcher.matches()) {
+                String delimiter = matcher.group(1);
+                String toSplit = matcher.group(2);
+                return  toSplit.split(delimiter);
+            }
+            throw new RuntimeException("Wrong regex for custom delimiter");
+        }
+        return delimiterAndNumbers.split(",|\n");
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,4 +1,6 @@
 import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
 class StringCalculatorTest {
@@ -16,13 +18,34 @@ class StringCalculatorTest {
     }
 
     @Test
-    void should_return_zero_for_uncoherence_string() {
-        assertThat(calculator.Add("2;5")).isEqualTo(0);
+    void should_return_throw_Exception_for_uncoherence_string() {
+        assertThatThrownBy(() ->calculator.Add("2;5"))
+                .isInstanceOf(NumberFormatException.class);
     }
 
     @Test
     void should_return_number_for_one_number() {
         assertThat(calculator.Add("5")).isEqualTo(5);
+    }
+
+    @Test
+    void should_handle_an_unknown_amount_of_numbers() {
+        assertThat(calculator.Add("1,2,3")).isEqualTo(6);
+    }
+
+    @Test
+    void should_allow_the_add_method_to_handle_new_lines_between_numbers() {
+        assertThat(calculator.Add("1\n2,3")).isEqualTo(6);
+    }
+
+    @Test
+    void should_support_different_delimiters() {
+        assertThat(calculator.Add("//;\n1;2")).isEqualTo(3);
+    }
+
+    @Test
+    void should_support_different_delimiters_2() {
+        assertThat(calculator.Add("//:\n1:2")).isEqualTo(3);
     }
 
 }
